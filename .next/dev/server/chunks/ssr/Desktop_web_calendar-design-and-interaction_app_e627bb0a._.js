e0c586d8fce333e18f61bc807348a03c65480370
barcke <barcke@163.com>
2025-12-22 21:22:54 +0800

    pref: 补充图片标题部分逻辑

---
+++ .next/dev/server/chunks/ssr/Desktop_web_calendar-design-and-interaction_app_e627bb0a._.js
@@ -0,0 +1,22 @@
+module.exports = [
+"[project]/Desktop/web/calendar-design-and-interaction/app/icon.png (static in ecmascript, tag client)", ((__turbopack_context__) => {
+
+__turbopack_context__.v("/_next/static/media/icon.314b6c18.png");}),
+"[project]/Desktop/web/calendar-design-and-interaction/app/icon.png.mjs { IMAGE => \"[project]/Desktop/web/calendar-design-and-interaction/app/icon.png (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$web$2f$calendar$2d$design$2d$and$2d$interaction$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__ = __turbopack_context__.i("[project]/Desktop/web/calendar-design-and-interaction/app/icon.png (static in ecmascript, tag client)");
+;
+const __TURBOPACK__default__export__ = {
+    src: __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$web$2f$calendar$2d$design$2d$and$2d$interaction$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
+    width: 1024,
+    height: 1024
+};
+}),
+];
+
+//# sourceMappingURL=Desktop_web_calendar-design-and-interaction_app_e627bb0a._.js.map
